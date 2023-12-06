61f9d9dc881fbc9f44948e9496ade08feee33b4d
IndigoCandle <romyannay1@gmail.com>
2023-12-06 10:34:21 +0200

    mul and sub added

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def multiply(first_num,second_num):
+    if first_num.isdigit() and second_num.isdigit:
+        return first_num * second_num
+    else:
+        print("Invalid - values must be numbers")
+        return None
+def subtract(first_num,second_num):
+    if first_num.isdigit() and second_num.isdigit:
+        return first_num - second_num
+    else:
+        print("Invalid - values must be numbers")
+        return None
